41e6f3b5d9901c02c0c1a55b9acb13b969b980e0
leslie27400 <33115409+leslie27400@users.noreply.github.com>
2020-05-27 13:04:39 +0200

    welcome

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,12 +4,9 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
-
         <title>{{ config('app.name', 'Laravel') }}</title>
-
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
         <script src="{{ asset('js/miapp.js') }}" defer></script>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,97 +1,106 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        @include('includes.head')
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))  
-            <!-- Pregunta si existe la ruta login  -->
-                <div class="top-right links">
-                    @auth
-                    <!-- Si iniciamos  -->
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                          <!-- si no es asi   -->
-                        <a href="{{ route('login') }}">Login</a>
+        <div class="d-flex justify-content-center" >
+            <div class="container">
+                <div class="row">
+                    <div class="d-flex justify-content-end col-md-12" >
+                        @if (Route::has('login'))  
+                        <!-- Pregunta si existe la ruta login  -->
+                        <div  class="register links">
+                            @auth
+                            <!-- Si iniciamos  -->
+                            <a href="{{ url('/home') }}"></a>
+                            @else
+                            <!-- si no es asi   -->
+                            <a href="{{ route('login') }}">Login</a>
 
-                        @if (Route::has('register'))
+                            @if (Route::has('register'))
                             <a href="{{ route('register') }}">Register</a>
+                            @endif
+                            @endauth
+                        </div>
                         @endif
-                    @endauth
+                    </div>
                 </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
+                @include('includes.header')
+                <div class="row">
+                    <div id="carousel-example-2" class="carousel slide carousel-fade col-md-12" data-ride="carousel">
+                        <!--Indicators-->
+                        <ol class="carousel-indicators">
+                            <li data-target="#carousel-example-2" data-slide-to="0" class="active"></li>
+                            <li data-target="#carousel-example-2" data-slide-to="1"></li>
+                            <li data-target="#carousel-example-2" data-slide-to="2"></li>
+                        </ol>
+                        <!--/.Indicators-->
+                        <!--Slides-->
+                        <div class="carousel-inner" role="listbox">
+                            <div class="carousel-item active">
+                                <div class="view">
+                                    <img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(68).jpg"
+                                         alt="First slide">
+                                    <div class="mask rgba-black-light"></div>
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <!--Mask color-->
+                                <div class="view">
+                                    <img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(6).jpg"
+                                         alt="Second slide">
+                                    <div class="mask rgba-black-strong"></div>
+                                </div>
+                               
+                            </div>
+                            <div class="carousel-item">
+                                <!--Mask color-->
+                                <div class="view">
+                                    <img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(9).jpg"
+                                         alt="Third slide">
+                                    <div class="mask rgba-black-slight"></div>
+                                </div>
+                            </div>
+                        </div>
+                        <!--/.Slides-->
+                        <!--Controls-->
+                        <a class="carousel-control-prev" href="#carousel-example-2" role="button" data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="carousel-control-next" href="#carousel-example-2" role="button" data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                        <!--/.Controls-->
+                    </div>
                 </div>
-
-                <div class="links">
-                    <a href="/admin">ADMINISTRADOR</a>
-                 
+                <div class="row">
+                    <div class="card col-md-12">
+                        <h5 class="card-header">Cartelera</h5>
+                        <div class="card-body">
+                            <h5 class="card-title">Special title treatment</h5>
+                            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </body>
+    </div>
+</body>
+<footer>
+    <div class="d-flex justify-content-center" >
+        <div class="container">
+             <div class="row">
+            @include('includes.footer')
+              </div>
+        </div>
+         
+         </div>
+   
+</footer>
+
+<!-- Footer -->
 </html>
